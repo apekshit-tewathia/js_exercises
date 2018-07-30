8c6bbf01c82ba83c256590438e8cce06d6b8c558
apekshit@vinsol.com <apekshittewathia@MacBook-Pro.local>
2018-07-30 11:27:53 +0530

    changes made as suggested

--- exercise1/user_class_exercise.js
+++ exercise1/user_class_exercise.js
@@ -3,16 +3,16 @@ function User(name, age) {
   this.age = age;
 }
 
-User.prototype.compare = function(user2) {
-  if (this.age > user2.age) {
-    return this.name + " is older than " + user2.name;
+User.prototype.compare = function(other_user) {
+  if (this.age > other_user.age) {
+    return this.name + " is older than " + other_user.name;
   }
-  if (this.age < user2.age) {
-    return user2.name + " is older than " + this.name;
+  if (this.age < other_user.age) {
+    return other_user.name + " is older than " + this.name;
   }
   return "They have equal ages";
 }
 
-var user1 = new User("Apekshit", 22);
-var user2 = new User("Rajneesh", 67);
-alert(user1.compare(user2));
+var user = new User("Apekshit", 22);
+var other_user = new User("Rajneesh", 67);
+alert(user.compare(other_user));
